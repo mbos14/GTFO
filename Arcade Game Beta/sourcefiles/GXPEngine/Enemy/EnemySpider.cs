e289257000c0b9afe9c4f01ab0d1157b977fac39
mbos14 <mbeetje@hotmail.com>
2015-11-23 13:00:40 +0100

    removing unnneded variables

--- Arcade Game Beta/sourcefiles/GXPEngine/Enemy/EnemySpider.cs
+++ Arcade Game Beta/sourcefiles/GXPEngine/Enemy/EnemySpider.cs
@@ -7,26 +7,23 @@ namespace GXPEngine
 {
     public class EnemySpider : Enemy
     {
-        private int _moveCounter = 0;
         private float _velocityX = 1.0f;
 
         //Animation
         private float _frame = 0.0f;
-        //private float _firstFrame;
-        //private float _lastFrame;
-
-        //private int _animState;
 
         public EnemySpider(Level pLevel) : base("robospider.png", 4, 3, pLevel)
         {
             SetOrigin(width / 2, height / 2);
             scaleX *= -1;
         }
+
         void Update()
         {
             animation();
             Move();
         }
+
         protected override void Move()
         {
             x += _velocityX; //Move
@@ -37,6 +34,7 @@ namespace GXPEngine
             _velocityX *= -1; //Move the other way
             scaleX *= -1; //Rescale
         }
+
         private void animation()
         {
             _frame += 0.2f;
