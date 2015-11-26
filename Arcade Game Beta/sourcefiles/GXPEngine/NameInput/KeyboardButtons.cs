c1b7304d6b80330fd5db1d7e73f2e23335f17bc4
mbos14 <mbeetje@hotmail.com>
2015-11-26 13:41:17 +0100

    no message

--- Arcade Game Beta/sourcefiles/GXPEngine/NameInput/KeyboardButtons.cs
+++ Arcade Game Beta/sourcefiles/GXPEngine/NameInput/KeyboardButtons.cs
@@ -12,7 +12,7 @@ namespace GXPEngine
         private int _buttonNumber;
         private string[] value = {"1","2","3","4","5","6","7","8","9","0","Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M","?","!","backspace","final"};
 
-        public KeyboardButtons(int pFrame, int pButtonValue) : base("KeyboardButtons2.png",10,8) //960 x 768 (10 columns, 8 rows)
+        public KeyboardButtons(int pFrame, int pButtonValue) : base("keyboardbuttons.png",10,8) //960 x 768 (10 columns, 8 rows)
         {
             thisFrame = pFrame;
             _buttonNumber = pButtonValue;
